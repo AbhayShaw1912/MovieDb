02fed010f24b215a48ea4b8d7ef842a241b7184f
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-03-16 00:14:49 +0530

    added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loading(props) {
+    return (
+        <div className="flex justify-center mt-16">
+            <img src="Blocks.svg" alt="loading"/>
+        </div>
+
+    );
+}
