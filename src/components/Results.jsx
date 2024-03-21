a110b765fe54f009824e38f74f2371b55fc7bd89
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-03-21 13:16:46 +0530

    fixed issue for non existing poster image

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Card from "@/components/Card";
 
 export default function Results({results}) {
+    const filteredResults = results.filter(result => result.backdrop_path !== null);
     return (
         <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl  mx-auto py-4'>
-            {results.map((results) => (
+            {filteredResults.map((results) => (
                 <Card key={results.id} results={results}/>
             ))}
         </div>
